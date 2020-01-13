035617670c3eed068b492a452165cd8be9b6467d
DrWei80 <drwei80@outlook.com>
2020-01-13 20:11:54 +0800

    Jedis操作字符串

--- Streaming/src/main/scala/redisConnectionTest2.scala
+++ Streaming/src/main/scala/redisConnectionTest2.scala
@@ -1,4 +1,4 @@
-import redis.clients.jedis.{JedisPool, JedisPoolConfig}
+import redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig}
 
 object redisConnectionTest2 {
   //利用连接池方式连接redis
@@ -18,6 +18,37 @@ object redisConnectionTest2 {
     jedis.auth("123456")
 
     println(jedis.ping())
+
+    stringDemo(jedis)
+
+  }
+  //操作字符串
+  def stringDemo(jedis:Jedis)={
+    jedis.set("name","胡锦涛")
+    println(jedis.get("name"))
+
+    jedis.append("name","很帅")
+    println(jedis.get("name"))
+
+    jedis.getSet("count","100")//替换
+    jedis.incr("count")
+    println(jedis.get("count"))
+    jedis.incrBy("count",99)
+    println(jedis.get("count"))
+
+
+    jedis.decr("count")
+    jedis.decrBy("count",38)
+    jedis.incrByFloat("count",0.009)//变成浮点数之后不能再操作整数
+    println(jedis.get("count"))
+
+    println(jedis.strlen("name"))//获取长度
+
+    jedis.mset("age","19","sex","女","address","北京天安门")
+
+    println(jedis.mget("name", "age", "sex", "address", "count"))//返回一个List
+
+
 
   }
 }
